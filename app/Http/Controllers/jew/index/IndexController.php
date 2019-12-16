b756f6070b97bc778d1301a36ac777fdd434ea6d
boss59 <2792593650@qq.com>
2019-12-16 14:25:01 +0800

    aa

--- app/Http/Controllers/jew/index/IndexController.php
+++ app/Http/Controllers/jew/index/IndexController.php
@@ -29,7 +29,7 @@ class IndexController extends Controller
         $showimg=$data['friend'];
         $str="";
         foreach ($showimg as $val){
-            $str.="<a href='{$val["f_url"]}'><img src='http://www.vonetxs.com{$val["f_logo"]}' /></a>";
+            $str.=" <div class='swiper-slide'><a href='{$val["f_url"]}'><img src='http://www.vonetxs.com{$val["f_logo"]}' /></a></div>";
         }
         $data['friend']=$str;
         return json_encode($data['friend'],JSON_UNESCAPED_UNICODE);
