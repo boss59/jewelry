28fe8acbb20aa48f55dd6723738656fbe10be1f4
boss59 <2792593650@qq.com>
2019-12-16 13:53:24 +0800

    aa

--- app/Http/Controllers/jew/index/IndexController.php
+++ app/Http/Controllers/jew/index/IndexController.php
@@ -24,7 +24,14 @@ class IndexController extends Controller
 
         //返回数据
         $data = ['friend'=>$friend,'sell'=>$sell,'top'=>$top_name,'goods'=>$floor['goods']];
-        return json_encode($data,JSON_UNESCAPED_UNICODE);
+
+        $showimg=$data['friend'];
+        $str="";
+        foreach ($showimg as $val){
+            $str.="<div class='swiper-wrapper'><div class='swiper-slide'><a href='{$val["f_url"]}'><img src='{$val["f_logo"]}' /></a></div></div><div class='swiper-pagination'></div>";
+        }
+        $data['friend']=$str;
+        return json_encode($data['friend'],JSON_UNESCAPED_UNICODE);
     }
 
     // 方法
