cf1ac0b3e1c7affc8edc0f4ab02aea38f2245323
boss59 <2792593650@qq.com>
2019-12-16 11:33:05 +0800

    aa

--- app/Http/Controllers/jew/index/IndexController.php
+++ app/Http/Controllers/jew/index/IndexController.php
@@ -7,11 +7,15 @@ use App\Http\Controllers\Controller;
 use App\Models\GoodsModel;
 use App\Models\CateModel;
 use App\Models\BrandModel;
+use App\Models\FriendModel;
 class IndexController extends Controller
 {
     // 前台首页
     public function index(Request $request)
     {
+        $friend=FriendModel::get()->toArray();
+
+        dd($friend);
         $cid=$request->input('cate_id');
     }
 }
