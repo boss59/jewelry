74a368cb0d65dcacb67329d7a2a39b034d86c18e
boss59 <2792593650@qq.com>
2019-12-17 22:40:36 +0800

    aa

--- app/Http/Controllers/jew/index/IndexController.php
+++ app/Http/Controllers/jew/index/IndexController.php
@@ -19,7 +19,7 @@ class IndexController extends Controller
         $sell=GoodsModel::where('is_up',1)->where('is_sell',1)->get()->toArray();
 
         //分类 下的商品
-        $top_name=CateModel::where('parent_id',0)->first()->toArray();
+        $top_name=CateModel::where('parent_id',0)->get()->toArray();
         $floor = $this->getFloot($top_name['cate_id']);
 
         //返回数据
