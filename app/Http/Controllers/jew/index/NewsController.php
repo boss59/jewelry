b9a0462dbf67945c17d6795732f38f2fa262e9f3
boss59 <2792593650@qq.com>
2019-12-18 12:17:54 +0800

    aa

--- app/Http/Controllers/jew/index/NewsController.php
+++ app/Http/Controllers/jew/index/NewsController.php
@@ -8,10 +8,17 @@ use App\Models\NewsModel;
 class NewsController extends Controller
 {
     // 新闻 列表
-    public function news()
+    public function news(Request $request)
     {
-        $new=NewsModel::get()->toArray();
-        $data = ['new'=>$new];
+        $new_id=$request->input('new_id');
+        dd($new_id);
+        if (empty($new_id)){
+            $new=NewsModel::get()->toArray();
+        }else{
+            $new=NewsModel::where('new_id',$new_id)->get()->toArray();
+        }
+
+        $data = ['news'=>$new];
         return json_encode($data,JSON_UNESCAPED_UNICODE);
     }
 }
