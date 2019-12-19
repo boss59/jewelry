cf3933c874c916562f20268a4c8e96c91e3c8d00
boss59 <2792593650@qq.com>
2019-12-19 23:10:50 +0800

    aa

--- app/Http/Controllers/jew/index/CollectController.php
+++ app/Http/Controllers/jew/index/CollectController.php
@@ -47,6 +47,7 @@ class CollectController extends Controller
     //收藏 展示============================================
     public function list_collect(Request $request)
     {
+        echo 1;exit;
         $user_id = $request->input('user_id');
         if (!empty($user_id)) {
             // 数据库
