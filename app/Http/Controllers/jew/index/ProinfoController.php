b43a74f76250a580850d7c57a49284a2acfa60a3
boss59 <2792593650@qq.com>
2019-12-20 23:22:47 +0800

    aa

--- app/Http/Controllers/jew/index/ProinfoController.php
+++ app/Http/Controllers/jew/index/ProinfoController.php
@@ -224,8 +224,8 @@ class ProinfoController extends Controller
         $gid = explode(',',$goods_id);
 //        echo json_encode($gid,JSON_UNESCAPED_UNICODE);exit;
         $info = CaryModel::where('user_id',$user_id)
-            ->whereIn('shop_cray.goods_id',$gid)
-            ->join('shop_goods','shop_cray.goods_id','=','shop_goods.goods_id')
+            ->whereIn('shop_cary.goods_id',$gid)
+            ->join('shop_goods','shop_goods.goods_id','=','shop_cary.goods_id')
             ->get()->toArray();
          dd($info);
         $count=number_format(0,2,'.','');
