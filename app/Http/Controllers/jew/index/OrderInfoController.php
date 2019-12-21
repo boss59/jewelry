fee5e572e5096acc9fc6f608052381b7bb554cc4
boss59 <2792593650@qq.com>
2019-12-21 20:12:44 +0800

    aa

--- app/Http/Controllers/jew/index/OrderInfoController.php
+++ app/Http/Controllers/jew/index/OrderInfoController.php
@@ -29,26 +29,29 @@ class OrderInfoController extends Controller
         // 根据 商品id 获取 商品信息
         $goods_id = $request->input('goods_id');
         $gid = explode(',',$goods_id);
-        // dd($gid);
-        //查询 商品
-        $goods = CaryModel::where('user_id',$user_id)
-            ->whereIn('shop_goods.goods_id',$gid)
-            ->join('shop_goods','shop_cary.goods_id','=','shop_goods.goods_id')
-            ->get()->toArray();
+        if(empty($gid)){
+            return json_encode(['code'=>2,'font'=>'无商品！']);
+        }else{
+            //查询 商品
+            $goods = CaryModel::where('user_id',$user_id)
+                ->whereIn('shop_goods.goods_id',$gid)
+                ->join('shop_goods','shop_cary.goods_id','=','shop_goods.goods_id')
+                ->get()->toArray();
 
-        // 收货地址
-        $address=AddressModel::where(['user_id'=>$user_id,'is_deff'=>1])->get()->toArray();
+            // 收货地址
+            $address=AddressModel::where(['user_id'=>$user_id,'is_deff'=>1])->get()->toArray();
 
-        // 总价钱
-        $info = CaryModel::where('user_id',$user_id)
-            ->whereIn('goods_id',$gid)
-            ->get(['add_price','buy_number'])->toArray();
-        $total = number_format(0,2,'.','');
-        foreach ($info as $k => $v) {
-            $total+=$v['add_price']*$v['buy_number'];
+            // 总价钱
+            $info = CaryModel::where('user_id',$user_id)
+                ->whereIn('goods_id',$gid)
+                ->get(['add_price','buy_number'])->toArray();
+            $total = number_format(0,2,'.','');
+            foreach ($info as $k => $v) {
+                $total+=$v['add_price']*$v['buy_number'];
+            }
+            $data = ['goods'=>$goods,'address'=>$address,'total'=>$total];
+            return json_encode($data,JSON_UNESCAPED_UNICODE);
         }
-        $data = ['goods'=>$goods,'address'=>$address,'total'=>$total];
-        return json_encode($data,JSON_UNESCAPED_UNICODE);
     }
 
 
