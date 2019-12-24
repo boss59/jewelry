67c1b53991fd9343694f3bae3e4de025912a3258
boss59 <2792593650@qq.com>
2019-12-24 10:53:05 +0800

    aa

--- app/Http/Controllers/jew/index/MineController.php
+++ app/Http/Controllers/jew/index/MineController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\CollectModel;
 use App\Models\HistoryModel;
+use App\Models\GoodsModel;
+use App\Models\OrderInfoModel;
 class MineController extends Controller
 {
     // 统计
@@ -14,12 +16,27 @@ class MineController extends Controller
         $user_id = $request->input('user_id');
         $where = ['user_id'=>$user_id];
         // 收藏
-        $collect=CollectModel::where($where)->count();
+            $collect ="";
+            $collect=CollectModel::where($where)->count();
         // 浏览历史
-        $collect=CollectModel::where($where)->count();
+            $history ="";
+            $data = HistoryModel::where($where)->get()->toArray();
+            $goods_id = [];
+            foreach($data as $k=>$v){
+                $goods_id[] = $v['goods_id'];
+            }
+            $history=GoodsModel::whereIn('goods_id',array_unique($goods_id))->count();
         // 全部订单
+            $order ="";
+            $order = OrderInfoModel::where($where)->count();
         // 已付款
+            $pay ="";
+            $pay = OrderInfoModel::where($where)->where('pay_status',2)->count();
         // 未付款
+            $Nopay ="";
+            $Nopay = OrderInfoModel::where($where)->where('pay_status',0)->count();
+        $res = ['collect'=>$collect,'history'=>$history,'order'=>$order,'pay'=>$pay,'Nopay'=>$Nopay];
+        return json_encode($res,JSON_UNESCAPED_UNICODE);
     }
 
 
