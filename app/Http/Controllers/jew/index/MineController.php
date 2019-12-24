41678a956abbca7fe11b515ab1d27c72071b2848
boss59 <2792593650@qq.com>
2019-12-24 20:08:29 +0800

    aa

--- app/Http/Controllers/jew/index/MineController.php
+++ app/Http/Controllers/jew/index/MineController.php
@@ -17,10 +17,9 @@ class MineController extends Controller
         $user_id = $request->input('user_id');
         $where = ['user_id'=>$user_id];
         // 收藏
-
             $collect=CollectModel::where($where)->count();
+            dd($collect);
         // 浏览历史
-
             $data = HistoryModel::where($where)->get()->toArray();
             $goods_id = [];
             foreach($data as $k=>$v){
@@ -28,16 +27,12 @@ class MineController extends Controller
             }
             $history=GoodsModel::whereIn('goods_id',array_unique($goods_id))->count();
         // 全部订单
-
             $order = OrderInfoModel::where($where)->count();
         // 已付款
-
             $pay = OrderInfoModel::where($where)->where('pay_status',2)->count();
         // 未付款
-
             $Nopay = OrderInfoModel::where($where)->where('pay_status',0)->count();
         //计算 购物车 数量
-
             $car = CaryModel::where($where)->count();
         $res = ['collect'=>$collect,'history'=>$history,'order'=>$order,'pay'=>$pay,'Nopay'=>$Nopay,'car'=>$car];
         return json_encode($res,JSON_UNESCAPED_UNICODE);
